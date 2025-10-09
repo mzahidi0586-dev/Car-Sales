b24ebb498e911baabffa0fd7e9f6ed38f9440f4d
mzahidi0586-dev <m.zahidi0586@gmail.com>
2025-10-09 23:06:24 +0000

    Updates

--- index.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import * as ReactDOM from "react-dom/client";
-import "./styles.css";
-import VehicleStockApp from "./vehicle-stocks-app";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(_jsx(VehicleStockApp, {}));
--- test.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const TestComponent = () => {
-    return _jsx("div", { children: "Hello, world!" });
-};
-export default TestComponent;
